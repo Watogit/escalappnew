3ea4641f6c1d079e115ae9e03594b1200042dd1c
Watogit <lopezpataquivawalter@artesyletras.edu.co>
2023-06-06 10:24:23 -0500

    mi segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,79 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule,Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { FiltrosComponent } from './filtros/filtros.component';
+import { CamaraComponent } from './camara/camara.component';
+import { OpcionesComponent } from './opciones/opciones.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+
+
+//la contante de mi Routermodule
+
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  {
+    path: 'landing',
+    component:LandingComponent,
+  },
+  {
+    path: 'filtros',
+    component:FiltrosComponent,
+  },
+  {
+    path: 'camara',
+    component:CamaraComponent,
+  },
+  {
+   path: 'opciones',
+   component: OpcionesComponent,
+  },
+  {
+    path: 'busqueda',
+    component: BusquedaComponent,
+  },
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch:'full',
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent,
+  }
+];
+
+/*-- los modulos d emi app--*/
+
+// landing
+//home
+//filtros
+//camara
+//opciones
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingComponent,
+    FiltrosComponent,
+    CamaraComponent,
+    OpcionesComponent,
+    PageNotFoundComponent,
+    BusquedaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
